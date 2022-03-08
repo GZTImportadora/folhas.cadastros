2e56fce53838ab699e0bc8fc3ed8fd1ae565b9b6
GZT Cadastros <98116029+GZTImportadora@users.noreply.github.com>
2022-03-08 14:22:23 -0300

    Update funcao_folha.js

--- css_js/funcao_folha.js
+++ css_js/funcao_folha.js
@@ -125,3 +125,15 @@ function verifica() {
 
 }
 
+function readImage() {
+    if (this.files && this.files[0]) {
+        var file = new FileReader();
+        file.onload = function(e) {
+            document.getElementById("preview").src = e.target.result;
+        };       
+        file.readAsDataURL(this.files[0]);
+    }
+}
+
+
+
